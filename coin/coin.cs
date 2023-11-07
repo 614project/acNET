d2073dc1038ba1ceca4768d6f88553eb5dbfd382
614project <71429443+614project@users.noreply.github.com>
2023-11-08 02:46:41 +0900

    코인/상점/환율 추가

---
+++ coin/coin.cs
@@ -0,0 +1,15 @@
+﻿using acNET.Type;
+
+namespace acNET.Coin
+{
+    /// <summary>
+    /// 코인->별조각 환율
+    /// </summary>
+    public class ExchangeRate : BaseBody
+    {
+        /// <summary>
+        /// 코인->별조각 환율입니다. 수수료 1%는 제외되어 있습니다.
+        /// </summary>
+        public int rate;
+    }
+}
---
+++ coin/item.cs
@@ -0,0 +1,33 @@
+﻿namespace acNET.Coin
+{
+    /// <summary>
+    /// 사용자가 사용할 수 있는 아이템입니다.
+    /// </summary>
+    public class Item
+    {
+        /// <summary>
+        /// 아이템의 ID입니다.
+        /// </summary>
+        public string itemId;
+        /// <summary>
+        /// 아이템 사진으로 가는 하이퍼링크입니다.
+        /// </summary>
+        public string itemImageUrl;
+        /// <summary>
+        /// 최대 소유 가능 개수입니다. 호출자에 따라 달라질 수 있습니다.
+        /// </summary>
+        public int inventoryMaxUnits;
+        /// <summary>
+        /// 아이템 사용 가능 여부입니다.
+        /// </summary>
+        public bool usable;
+        /// <summary>
+        /// 아이템의 이름입니다.
+        /// </summary>
+        public string displayName;
+        /// <summary>
+        /// 아이템의 설명입니다.
+        /// </summary>
+        public string displayDescription;
+    }
+}
