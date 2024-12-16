5a1f4a98195faa74d1eb55fbba4a3100a52ebeef
Jyunni <71429443+614project@users.noreply.github.com>
2024-12-16 23:53:37 +0900

    version up

--- acNET.cs
+++ acNET.cs
@@ -6,13 +6,11 @@
  * copyright 614project.
  */
 
-#if DEBUG
 /*
- * 미리 정의된 IsExternalInit가 없다는 오류 <<< vs2022 버그
+ * 미리 정의된 IsExternalInit가 없다는 오류 <<< vs2022 버그...? 일지도
  * https://stackoverflow.com/questions/64749385/predefined-type-system-runtime-compilerservices-isexternalinit-is-not-defined
  */
 namespace System.Runtime.CompilerServices
 {
     internal static class IsExternalInit { }
 }
-#endif
