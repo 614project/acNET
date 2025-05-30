25e086544071245f813d1eb1e5ecdad864818e32
Jyunni <71429443+614project@users.noreply.github.com>
2025-05-30 21:12:32 +0900

    솔브드 이벤트 api 추가

---
+++ event/SolvedDecorateSummerPostcardEvent.cs
@@ -0,0 +1,76 @@
+﻿using AcNET.Type;
+using Newtonsoft.Json;
+
+namespace AcNET.Events;
+
+/// <summary>
+/// 2022년 6월 26일에 열린 여름 엽서 꾸미기 이벤트
+/// </summary>
+public class SolvedDecorateSummerPostcardEvent : Jsonable
+{
+    [JsonProperty("highestRecord")]
+    public RecordStatus HighestRecord { get; set; } = null;
+
+    [JsonProperty("equipStatus")]
+    public Equips EquipStatus { get; set; } = null;
+
+    [JsonProperty("items")]
+    public List<ProblemItem> Items { get; set; } = null;
+
+    [JsonProperty("result")]
+    public ResultStat Result { get; set; } = null;
+
+    public class ProblemItem
+    {
+        [JsonProperty("problemId")]
+        public int ProblemId { get; set; }
+
+        [JsonProperty("score")]
+        public int Score { get; set; }
+
+        [JsonProperty("level")]
+        public int Level { get; set; }
+
+        [JsonProperty("isInLevel")]
+        public string IsInLevel { get; set; }
+
+        [JsonProperty("tags")]
+        public List<string> Tags { get; set; } = null;
+
+        [JsonProperty("shape")]
+        public int Shape { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("acceptedUserCount")]
+        public int AcceptedUserCount { get; set; }
+
+        [JsonProperty("equipped")]
+        public bool Equipped { get; set; }
+    }
+
+    public class Equips
+    {
+        [JsonProperty("equipItems")]
+        public List<ProblemItem> EquipItems { get; set; } = null;
+    }
+
+    public class RecordStatus
+    {
+        [JsonProperty("score")]
+        public int Score { get; set; }
+
+        [JsonProperty("equipStatus")]
+        public Equips EquipStatus { get; set; } = null;
+    }
+
+    public class ResultStat
+    {
+        [JsonProperty("isWinner")]
+        public bool IsWinner { get; set; }
+
+        [JsonProperty("wonStardusts")]
+        public int WonStardusts { get; set; }
+    }
+}
---
+++ event/SolvedPeperoDay2021Event.cs
@@ -0,0 +1,67 @@
+﻿using AcNET.Type;
+using Newtonsoft.Json;
+
+namespace AcNET.Events;
+
+/// <summary>
+/// 2021년 11월 11일에 열린 빼빼로 데이 이벤트
+/// </summary>
+public class SolvedPeperoDay2021Event : Jsonable
+{
+    public class Cost
+    {
+        [JsonProperty("from")]
+        public string From { get; set; } = string.Empty;
+
+        [JsonProperty("to")]
+        public string To { get; set; } = string.Empty;
+
+        [JsonProperty("rate")]
+        public int Rate { get; set; }
+    }
+
+    [JsonProperty("base")]
+    public int Base { get; set; }
+
+    [JsonProperty("cream")]
+    public int Cream { get; set; }
+
+    [JsonProperty("choco")]
+    public int Choco { get; set; }
+
+    [JsonProperty("mint")]
+    public int Mint { get; set; }
+
+    [JsonProperty("ruby")]
+    public int Ruby { get; set; }
+
+    [JsonProperty("cookieCream")]
+    public int CookieCream { get; set; }
+
+    [JsonProperty("cookieChoco")]
+    public int CookieChoco { get; set; }
+
+    [JsonProperty("cookieMint")]
+    public int CookieMint { get; set; }
+
+    [JsonProperty("cookieRuby")]
+    public int CookieRuby { get; set; }
+
+    [JsonProperty("stardusts")]
+    public int Stardusts { get; set; }
+
+    [JsonProperty("gifticon")]
+    public string Gifticon { get; set; } = string.Empty;
+
+    [JsonProperty("cookie")]
+    public int Cookie { get; set; }
+
+    [JsonProperty("cookieRank")]
+    public int CookieRank { get; set; }
+
+    [JsonProperty("cookiePercentile")]
+    public int CookiePercentile { get; set; }
+
+    [JsonProperty("costs")]
+    public List<Cost> Costs { get; set; } = null;
+}
